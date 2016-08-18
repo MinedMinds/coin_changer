977bb2431d9a10cc00e81c5a9f5efc15530344ae
Jonathan Graham <jgraham@8thlight.com>
2016-08-18 09:59:03 -0400

    tests for 6 and 7 cents

--- test_coin_changer.rb
+++ test_coin_changer.rb
@@ -13,4 +13,20 @@ class TestCoinChanger < Minitest::Test
 	def test_two_cents_returns_hash_with_two_penny
 		assert_equal({:penny => 2}, generate_change(2))
 	end
+
+	def test_five_cents_returns_hash_with_one_nickel
+		assert_equal({:nickel => 1}, generate_change(5))
+	end
+
+	def test_six_cents_returns_hash_with_1_nickel_1_penny
+		assert_equal({:nickel => 1, :penny => 1}, generate_change(6))
+	end
+
+	def test_seven_cents_returns_hash_with_1_nickel_2_penny
+		assert_equal({:nickel => 1, :penny => 2}, generate_change(7))
+	end
+
+	# def test_ten_cents_returns_hash_with_two_nickel
+	# 	assert_equal({:nickel => 2}, generate_change(10))
+	# end
 end
