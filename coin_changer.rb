7ebb43a51d2b0430eb213238145124633a5e8e96
Jonathan Graham <jgraham@8thlight.com>
2016-08-18 11:08:43 -0400

    refactor

--- coin_changer.rb
+++ coin_changer.rb
@@ -1,23 +1,14 @@
 def generate_change(cents)
 	change = {}
-	if cents > 15
+	if cents >= 10
 		change[:dime] = 1
+		cents = cents - 10
+	end
+	if cents >= 5
 		change[:nickel] = 1
-		change[:penny] = cents - 15
-	elsif cents == 15
-		change[:dime] = 1
-		change[:nickel] = 1
-	elsif cents > 10
-		change[:dime] = 1
-		change[:penny] = cents - 10
-	elsif cents == 10
-		change[:dime] = 1
-	elsif cents > 5
-		change[:nickel] = 1
-		change[:penny] = cents - 5
-	elsif cents == 5
-		change[:nickel] = 1
-	elsif cents > 0
+		cents = cents - 5
+	end
+	if cents > 0
 		change[:penny] = cents
 	end
 	change
